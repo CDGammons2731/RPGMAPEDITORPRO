1717cbc5d63bed8756d696a50410864803892b58
Caleb Dan Gammons <alldayhat@hotmail.com>
2018-10-22 18:28:53 -0500

    Created Terrain Editing Script
    Created a script for editing our terrain.
    You can now edit the length and the width of the terrain in game for which the DM will be able to size his dungeon, city or land to fit his needs.

---
+++ RPGAdventurePro/Assets/TerrainEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainEditor : MonoBehaviour {
+    [Header("Terrain to edit")]
+    public Terrain terrain;
+    [Header("Terrain width, height, and length")]
+    public int x = 500;
+    public int y = 1;
+    public int z = 500;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TerrainSizeCheck();
+	}
+
+    void TerrainSizeCheck()
+    {
+        terrain.terrainData.size = new Vector3(x, y, z);
+    }
+
+}
