60fcdae03b967b0306121cc724e97fa160780ef1
Caleb Dan Gammons <alldayhat@hotmail.com>
2018-10-23 13:41:15 -0500

    Changes to TerrainEditor
    Changes were made last night to prepare for the terrain editor being able to manipulate the terrain's height at runtime.

--- RPGAdventurePro/Assets/TerrainEditor.cs
+++ RPGAdventurePro/Assets/TerrainEditor.cs
@@ -6,23 +6,42 @@ public class TerrainEditor : MonoBehaviour {
     [Header("Terrain to edit")]
     public Terrain terrain;
     [Header("Terrain width, height, and length")]
-    public int x = 500;
-    public int y = 1;
-    public int z = 500;
+    public int width = 500;
+    public int height = 1;
+    public int length = 500;
+    [Header("Raise or Lower Terrain")]
+    public bool RaiseLowerTerrain = false;
+    [Header("Height to raise and/or lower")]
+    public int Height = 1;
+    public Vector3 Mouse;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Mou();
         TerrainSizeCheck();
+
+        TerrainRaiseOrLower();
 	}
+
+    void Mou()
+    {
+        Mouse = Input.mousePosition;
+    }
 
     void TerrainSizeCheck()
     {
-        terrain.terrainData.size = new Vector3(x, y, z);
+        terrain.terrainData.size = new Vector3(width, height, length);
     }
+
+    void TerrainRaiseOrLower()
+    {
+        
+    }
+
 
 }
